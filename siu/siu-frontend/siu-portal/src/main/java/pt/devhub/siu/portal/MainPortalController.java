f0ea397107721dd838e4aa05f7bc8edb4934edba
António Ribeiro <antonio.romaneiro.ribeiro@gmail.com>
2015-10-07 23:05:23 +0100

    Added the SIU backend core module.

---
+++ siu/siu-frontend/siu-portal/src/main/java/pt/devhub/siu/portal/MainPortalController.java
@@ -0,0 +1,11 @@
+package pt.devhub.siu.portal;
+
+import javax.inject.Named;
+
+import org.apache.myfaces.extensions.cdi.core.api.scope.conversation.ViewAccessScoped;
+
+@Named
+@ViewAccessScoped
+public class MainPortalController {
+
+}
