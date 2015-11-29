1685526974a5475278897782ad3865f2c059fd9f
António Ribeiro <antonio.romaneiro.ribeiro@gmail.com>
2015-11-30 00:12:14 +0100

    Added the siu-common project.

---
+++ siu/siu-common/siu-common-entity/src/main/java/pt/devhub/siu/common/entity/IResponse.java
@@ -0,0 +1,10 @@
+package pt.devhub.siu.common.entity;
+
+import java.io.Serializable;
+
+/**
+ * Common interface for all external API responses.
+ */
+public interface IResponse extends Serializable {
+
+}
---
+++ siu/siu-common/siu-common-entity/src/main/java/pt/devhub/siu/common/entity/response/NasaResponse.java
@@ -0,0 +1,154 @@
+package pt.devhub.siu.common.entity.response;
+
+import pt.devhub.siu.common.entity.IResponse;
+
+/**
+ * Object that contains the data that represents the NASA API response.
+ */
+public class NasaResponse implements IResponse {
+
+	/**
+	 * The serial version unique identifier.
+	 */
+	private static final long serialVersionUID = 6265999069522152794L;
+
+	/**
+	 * NASA's picture of the day URL.
+	 */
+	private final String podUrl;
+	/**
+	 * NASA's picture of the day title.
+	 */
+	private final String title;
+	/**
+	 * NASA's picture of the day mediaType.
+	 */
+	private final String mediaType;
+	/**
+	 * NASA's picture of the day explanation.
+	 */
+	private final String explanation;
+
+	/**
+	 * Constructor for this class.
+	 * 
+	 * @param builder
+	 *            the builder class
+	 */
+	private NasaResponse(final NasaResponseBuilder builder) {
+		this.podUrl = builder.podUrl;
+		this.title = builder.title;
+		this.mediaType = builder.mediaType;
+		this.explanation = builder.explanation;
+	}
+
+	/**
+	 * @return the podUrl
+	 */
+	public String getPodUrl() {
+		return podUrl;
+	}
+
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+
+	/**
+	 * @return the mediaType
+	 */
+	public String getMediaType() {
+		return mediaType;
+	}
+
+	/**
+	 * @return the explanation
+	 */
+	public String getExplanation() {
+		return explanation;
+	}
+
+	/**
+	 * Builder for this class.
+	 */
+	public static final class NasaResponseBuilder {
+
+		/**
+		 * NASA's picture of the day URL.
+		 */
+		private String podUrl;
+		/**
+		 * NASA's picture of the day title.
+		 */
+		private String title;
+		/**
+		 * NASA's picture of the day media type.
+		 */
+		private String mediaType;
+		/**
+		 * NASA's picture of the day explanation.
+		 */
+		private String explanation;
+
+		/**
+		 * Default builder constructor.
+		 * 
+		 * @param podUrl
+		 *            NASA's picture of the day URL
+		 */
+		public NasaResponseBuilder(final String podUrl) {
+			this.podUrl = podUrl;
+		}
+
+		/**
+		 * Sets NASA's picture of the day title.
+		 * 
+		 * @param title
+		 *            NASA's picture of the day title
+		 * @return this builder
+		 */
+		public NasaResponseBuilder setTitle(String title) {
+			this.title = title;
+
+			return this;
+		}
+
+		/**
+		 * Sets NASA's picture of the day mediaType.
+		 * 
+		 * @param mediaType
+		 *            NASA's picture of the day mediaType
+		 * @return this builder
+		 */
+		public NasaResponseBuilder setMediaType(String mediaType) {
+			this.mediaType = mediaType;
+
+			return this;
+		}
+
+		/**
+		 * Sets NASA's picture of the day explanation.
+		 * 
+		 * @param explanation
+		 *            NASA's picture of the day explanation.
+		 * @return this builder
+		 */
+		public NasaResponseBuilder setExplanation(String explanation) {
+			this.explanation = explanation;
+
+			return this;
+		}
+
+		/**
+		 * Creates a new instance of the NasaResponse class in accordance with
+		 * the builder pattern.
+		 * 
+		 * @return a new instance of the NasaResponse class
+		 */
+		public NasaResponse build() {
+			return new NasaResponse(this);
+		}
+	}
+}
