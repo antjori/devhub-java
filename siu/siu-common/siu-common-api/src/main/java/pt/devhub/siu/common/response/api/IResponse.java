397af5f0c36c36fe75dfb61ee5f526ec12aa45ec
António Ribeiro <antonio.romaneiro.ribeiro@gmail.com>
2015-12-13 01:57:12 +0100

    Re-factored the project associated with SIU's external resources.

---
+++ siu/siu-common/siu-common-api/src/main/java/pt/devhub/siu/common/response/api/IResponse.java
@@ -0,0 +1,10 @@
+package pt.devhub.siu.common.response.api;
+
+import java.io.Serializable;
+
+/**
+ * Common interface for all external API responses.
+ */
+public interface IResponse extends Serializable {
+
+}
