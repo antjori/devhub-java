b669f7ed760605b3c63d72f68f78ed5b5b7fa177
António Ribeiro <antonio.romaneiro.ribeiro@gmail.com>
2016-03-29 00:00:23 +0100

    Added the solutions for problem 7 of Project-Euler.

---
+++ project-euler/src/main/java/pt/devhub/euler/problem/impl/EulerProblem7.java
@@ -0,0 +1,28 @@
+package pt.devhub.euler.problem.impl;
+
+import pt.devhub.euler.problem.EulerProblemSolverUtil;
+import pt.devhub.euler.problem.IEulerProblem;
+
+/**
+ * 10001st prime Problem 7 By listing the first six prime numbers: 2, 3, 5, 7,
+ * 11, and 13, we can see that the 6th prime is 13.
+ * 
+ * What is the 10 001st prime number?
+ */
+public class EulerProblem7 implements IEulerProblem {
+
+	@Override
+	public void solveProblem() {
+		int nPrimes = 1;
+		int number = 1;
+
+		while (nPrimes < 10001) {
+			number += 2;
+			if (EulerProblemSolverUtil.isPrime(number)) {
+				nPrimes++;
+			}
+		}
+
+		EulerProblemSolverUtil.printSolution(EulerProblem7.class, number);
+	}
+}
