44d27ea474165a7bb7fcab0f9b62454b6d0c2be5
António Ribeiro <antonio.romaneiro.ribeiro@gmail.com>
2016-03-30 21:23:25 +0100

    Added the base solution for problem 9 of Project-Euler.

--- project-euler/src/main/java/pt/devhub/euler/problem/EulerProblemSolver.java
+++ project-euler/src/main/java/pt/devhub/euler/problem/EulerProblemSolver.java
@@ -8,6 +8,7 @@ import pt.devhub.euler.problem.impl.EulerProblem4;
 import pt.devhub.euler.problem.impl.EulerProblem5;
 import pt.devhub.euler.problem.impl.EulerProblem6;
 import pt.devhub.euler.problem.impl.EulerProblem7;
+import pt.devhub.euler.problem.impl.EulerProblem9;
 
 /**
  * The Euler problem solver.
@@ -22,6 +23,7 @@ public class EulerProblemSolver {
 		problems.add(new EulerProblem5());
 		problems.add(new EulerProblem6());
 		problems.add(new EulerProblem7());
+		problems.add(new EulerProblem9());
 	}
 
 	public static void main(String[] args) {
---
+++ project-euler/src/main/java/pt/devhub/euler/problem/impl/EulerProblem9.java
@@ -0,0 +1,12 @@
+package pt.devhub.euler.problem.impl;
+
+import pt.devhub.euler.problem.IEulerProblem;
+
+public class EulerProblem9 implements IEulerProblem {
+
+	@Override
+	public void solveProblem() {
+
+	}
+
+}
