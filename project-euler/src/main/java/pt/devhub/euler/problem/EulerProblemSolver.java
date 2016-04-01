b03e057e277b512542e1aba57a7f7f8192fffe77
António Ribeiro <antonio.romaneiro.ribeiro@gmail.com>
2016-04-01 23:59:09 +0100

    Added missing solutions for Euler problems 1, 2 and 8.

--- project-euler/src/main/java/pt/devhub/euler/problem/EulerProblemSolver.java
+++ project-euler/src/main/java/pt/devhub/euler/problem/EulerProblemSolver.java
@@ -3,11 +3,14 @@ package pt.devhub.euler.problem;
 import java.util.ArrayList;
 import java.util.List;
 
+import pt.devhub.euler.problem.impl.EulerProblem1;
+import pt.devhub.euler.problem.impl.EulerProblem2;
 import pt.devhub.euler.problem.impl.EulerProblem3;
 import pt.devhub.euler.problem.impl.EulerProblem4;
 import pt.devhub.euler.problem.impl.EulerProblem5;
 import pt.devhub.euler.problem.impl.EulerProblem6;
 import pt.devhub.euler.problem.impl.EulerProblem7;
+import pt.devhub.euler.problem.impl.EulerProblem8;
 import pt.devhub.euler.problem.impl.EulerProblem9;
 
 /**
@@ -18,11 +21,14 @@ public class EulerProblemSolver {
 	private static List<IEulerProblem> problems = new ArrayList<>();
 
 	static {
+		problems.add(new EulerProblem1());
+		problems.add(new EulerProblem2());
 		problems.add(new EulerProblem3());
 		problems.add(new EulerProblem4());
 		problems.add(new EulerProblem5());
 		problems.add(new EulerProblem6());
 		problems.add(new EulerProblem7());
+		problems.add(new EulerProblem8());
 		problems.add(new EulerProblem9());
 	}
 
---
+++ project-euler/src/main/java/pt/devhub/euler/problem/impl/EulerProblem8.java
@@ -0,0 +1,68 @@
+package pt.devhub.euler.problem.impl;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.TreeMap;
+
+import pt.devhub.euler.problem.IEulerProblem;
+
+/**
+ * Largest product in a series Problem 8 The four adjacent digits in the
+ * 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
+ * 
+ * Find the thirteen adjacent digits in the 1000-digit number that have the
+ * greatest product. What is the value of this product?
+ */
+public class EulerProblem8 implements IEulerProblem {
+
+	private static String DIGITS;
+
+	static {
+		InputStream inputStream = EulerProblem8.class.getClassLoader()
+				.getResourceAsStream(EulerProblem8.class.getSimpleName().toLowerCase() + ".properties");
+		Properties properties = new Properties();
+
+		try {
+			properties.load(inputStream);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		TreeMap<Object, Object> digitsMap = new TreeMap<>(properties);
+		StringBuilder builder = new StringBuilder();
+
+		for (Object key : digitsMap.keySet()) {
+			builder.append(properties.getProperty((String) key));
+		}
+
+		DIGITS = builder.toString();
+	}
+
+	@Override
+	public void solveProblem() {
+		long result = 0;
+
+		for (int start = 0, end = DIGITS.length() - 1; (start + 13) < (end - 13); start++, end--) {
+			long startProduct = getProduct(DIGITS.substring(start, start + 13));
+			long endProduct = getProduct(DIGITS.substring(end - 13, end));
+			long possibleResult = startProduct > endProduct ? startProduct : endProduct;
+
+			if (possibleResult > result) {
+				result = possibleResult;
+			}
+		}
+
+		System.out.println(result);
+	}
+
+	private long getProduct(String number) {
+		long result = 1;
+
+		for (int it = 0; it < number.length(); it++) {
+			result *= Long.valueOf(String.valueOf(number.charAt(it)));
+		}
+
+		return result;
+	}
+}
