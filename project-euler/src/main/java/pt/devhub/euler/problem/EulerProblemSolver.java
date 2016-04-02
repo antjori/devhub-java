3624d04d5eec6cbe16b9a4e57a284a21c3ac997a
António Ribeiro <antonio.romaneiro.ribeiro@gmail.com>
2016-04-03 00:26:52 +0100

    Added instantiation of problem classes by reflection.

--- project-euler/src/main/java/pt/devhub/euler/problem/EulerProblemSolver.java
+++ project-euler/src/main/java/pt/devhub/euler/problem/EulerProblemSolver.java
@@ -3,16 +3,6 @@ package pt.devhub.euler.problem;
 import java.util.ArrayList;
 import java.util.List;
 
-import pt.devhub.euler.problem.impl.EulerProblem1;
-import pt.devhub.euler.problem.impl.EulerProblem2;
-import pt.devhub.euler.problem.impl.EulerProblem3;
-import pt.devhub.euler.problem.impl.EulerProblem4;
-import pt.devhub.euler.problem.impl.EulerProblem5;
-import pt.devhub.euler.problem.impl.EulerProblem6;
-import pt.devhub.euler.problem.impl.EulerProblem7;
-import pt.devhub.euler.problem.impl.EulerProblem8;
-import pt.devhub.euler.problem.impl.EulerProblem9;
-
 /**
  * The Euler problem solver.
  */
@@ -21,15 +11,14 @@ public class EulerProblemSolver {
 	private static List<IEulerProblem> problems = new ArrayList<>();
 
 	static {
-		problems.add(new EulerProblem1());
-		problems.add(new EulerProblem2());
-		problems.add(new EulerProblem3());
-		problems.add(new EulerProblem4());
-		problems.add(new EulerProblem5());
-		problems.add(new EulerProblem6());
-		problems.add(new EulerProblem7());
-		problems.add(new EulerProblem8());
-		problems.add(new EulerProblem9());
+		try {
+			for (int problemNum = 1; problemNum <= 9; problemNum++) {
+				problems.add((IEulerProblem) Class.forName("pt.devhub.euler.problem.impl.EulerProblem" + problemNum)
+						.newInstance());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static void main(String[] args) {
