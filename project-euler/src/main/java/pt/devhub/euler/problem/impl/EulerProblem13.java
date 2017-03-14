232657d303ed72242d8c020af34d2748e9a1e695
António Ribeiro <antonio.romaneiro.ribeiro@gmail.com>
2017-03-14 23:41:09 +0000

    Added problem 13 class.

---
+++ project-euler/src/main/java/pt/devhub/euler/problem/impl/EulerProblem13.java
@@ -0,0 +1,13 @@
+package pt.devhub.euler.problem.impl;
+
+import pt.devhub.euler.problem.IEulerProblem;
+
+public class EulerProblem13 implements IEulerProblem {
+
+	@Override
+	public void solveProblem() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
