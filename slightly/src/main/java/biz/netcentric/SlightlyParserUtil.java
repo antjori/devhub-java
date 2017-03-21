69f964b33e2ea4a51f65b4989d6712f1f1d73728
António Ribeiro <antonio.romaneiro.ribeiro@gmail.com>
2017-03-21 13:06:24 +0000

    Added utility class for Slightly parser.

---
+++ slightly/src/main/java/biz/netcentric/SlightlyParserUtil.java
@@ -0,0 +1,15 @@
+package biz.netcentric;
+
+/**
+ * Utility class for the Slightly parser.
+ */
+public final class SlightlyParserUtil {
+
+	public static final String ENCODING = "UTF-8";
+	public static final String SCRIPT = "SCRIPT";
+	public static final String SCRIPT_TYPE = "type";
+	public static final String SCRIPT_TYPE_VAL = "server/javascript";
+	public static final String JS_ENGINE = "nashorn";
+	public static final String ENABLE_RHINO = "load(\"nashorn:mozilla_compat.js\");";
+	public static final String SLIGHTLY_PACKAGE = "biz.netcentric.";
+}
