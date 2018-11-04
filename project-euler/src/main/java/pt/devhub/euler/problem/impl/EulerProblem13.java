4e761f0ea36509bd74f316b157cfc0a03019ad3f
Antonio Ribeiro <antonio.ribeiro@mercedes-benz.io>
2018-11-04 17:27:35 +0000

    Add implementation for Euler problem #13

--- project-euler/src/main/java/pt/devhub/euler/problem/impl/EulerProblem13.java
+++ project-euler/src/main/java/pt/devhub/euler/problem/impl/EulerProblem13.java
@@ -1,13 +1,18 @@
 package pt.devhub.euler.problem.impl;
 
+import java.util.Map;
+
+import pt.devhub.euler.problem.EulerProblemSolverUtil;
 import pt.devhub.euler.problem.IEulerProblem;
 
 public class EulerProblem13 implements IEulerProblem {
 
+	private static Map<Object, Object> LARGE_SUM = EulerProblemSolverUtil.loadProperties(EulerProblem13.class);
+
 	@Override
 	public void solveProblem() {
 		// TODO Auto-generated method stub
-		
+		// LARGE_SUM.values().stream().
 	}
 
 }
